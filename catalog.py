89c1410e86ca2c79b5ea03947c888ac1b9a4b195
Chad Lauthers <clauthers@gmail.com>
2018-07-12 18:52:44 -0400

    changed for python3

--- catalog.py
+++ catalog.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 from flask import redirect, jsonify, url_for, flash
-import psycopg2
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Category, Base, ItemCatalog, User
@@ -16,7 +15,7 @@ import requests
 
 app = Flask(__name__)
 
-engine = create_engine('postgresql://catalog:udacity@localhost:5432/catalog')
+engine = create_engine('sqlite:///catalog.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
@@ -31,7 +30,7 @@ APPLICATION_NAME = "Catalog Application"
 @app.route('/login')
 def showLogin():
     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
-                    for x in xrange(32))
+                    for x in range(32))
     login_session['state'] = state
     return render_template('login.html', STATE=state)
 
@@ -62,7 +61,8 @@ def gconnect():
     url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
            % access_token)
     h = httplib2.Http()
-    result = json.loads(h.request(url, 'GET')[1])
+    body = h.request(url, 'GET')
+    result = json.loads(body[1].decode('utf-8'))
     # If there was an error in the access token info, abort.
     if result.get('error') is not None:
         response = make_response(json.dumps(result.get('error')), 500)
@@ -81,7 +81,7 @@ def gconnect():
     if result['issued_to'] != CLIENT_ID:
         response = make_response(
             json.dumps("Token's client ID does not match app's."), 401)
-        print "Token's client ID does not match app's."
+        print("Token's client ID does not match app's.")
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -124,14 +124,13 @@ def gconnect():
     output += login_session['picture']
     output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '                                     # NOQA
     flash("you are now logged in as %s" % login_session['username'])
-    print "done!"
+    print("done!")
     return output
 
 # User Helper Functions
 
 
 def createUser(login_session):
-    print login_session
     newUser = User(name=login_session['username'], email=login_session[
                    'email'], picture=login_session['picture'])
     session.add(newUser)
@@ -314,5 +313,5 @@ def disconnect():
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
-    app.debug = False
-    app.run()
+    app.debug = True
+    app.run(host='0.0.0.0', port=8000)
