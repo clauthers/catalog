d41b58d27d538144ea26e5a62be22f967de7f123
Chad Lauthers <clauthers@gmail.com>
2018-07-12 19:21:35 -0400

    added .

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ from flask import redirect, jsonify, url_for, flash
 from sqlalchemy import create_engine, asc
 import psycopg2
 from sqlalchemy.orm import sessionmaker
-from database_setup import Category, Base, ItemCatalog, User
+from .database_setup import Category, Base, ItemCatalog, User
 from flask import session as login_session
 import random
 import string
